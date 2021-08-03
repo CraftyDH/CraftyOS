946d926ba43688686c6241ffabf4900f325ae5f9
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2021-08-03 12:28:22 +1000

    Add cargo build function

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # CraftyOS
 
-## Run with
+## To run steps
+### 1. OVMF   
+    Grab OVMF-pure-efi.fd and place it in the OVMF folder with that **exact** name. 
+    You can get this file from https://github.com/rust-osdev/ovmf-prebuilt
+
+### 2. Run
+    Execute cargo run in the root folder. This will automatically build the kernel and bootloader into a fat img then run qemu.
 ```sh
-cd builder
 cargo run
 ```
--- builder/src/main.rs
+++ builder/src/main.rs
@@ -1,4 +1,5 @@
 extern crate anyhow;
+extern crate cargo_metadata;
 extern crate fatfs;
 extern crate fscommon;
 
@@ -7,12 +8,17 @@ use std::fs::{self, File};
 use std::io::{self, BufReader};
 use std::process::{Command, Stdio};
 
-use cargo_metadata::Message;
+use cargo_metadata::{Message, camino};
 
 use fatfs::{format_volume, FileSystem, FormatVolumeOptions, FsOptions, StdIoWrapper};
 use fscommon::BufStream;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
+    // Compile all the stuff
+    let bootloaderpath = buildcargo("bootloader")?;
+    let kernelpath = buildcargo("kernel")?;
+
+    // Open and format the virtual fat disk
     let crafty_img = fs::OpenOptions::new()
         .read(true)
         .write(true)
@@ -20,84 +26,33 @@ fn main() -> anyhow::Result<()> {
         .truncate(true)
         .open("CraftyOS.img")?;
 
+    // 10 MB
     crafty_img.set_len(10 * 1024 * 1024)?;
-
     format_volume(
         &mut StdIoWrapper::from(&crafty_img),
-        FormatVolumeOptions::new().volume_label(*b"CraftyOS   "),
+        FormatVolumeOptions::new().volume_label(*b"CraftyOS..."),
     )?;
-    println!("Formated");
 
+    // Open root partition
     let partition = FileSystem::new(
         BufStream::new(&crafty_img),
         FsOptions::new().update_accessed_date(true),
     )?;
     let root = partition.root_dir();
-    // let testdir = fs.root_dir().create_dir("test")?;
 
-    // let mut file = partition.root_dir().create_file("hello.txt")?;
-
-    //// ! Get building to work https://github.com/rust-lang/cargo/issues/9451
-    // let mut command = Command::new("cargo")
-    //     .args(&[
-    //         "build",
-    //         "--workspace",
-    //         "--exclude",
-    //         "builder",
-    //         "--message-format=json-render-diagnostics",
-    //     ])
-    //     .stdout(Stdio::piped())
-    //     .spawn()
-    //     .unwrap();
-
+    // Create basic directories
     root.create_dir("/EFI")?;
     root.create_dir("/EFI/BOOT")?;
 
-    let names = ["../bootloader", "../kernel"];
+    // Copy all the files
+    let mut file = root.create_file("/EFI/BOOT/BootX64.EFI")?;
+    io::copy(&mut File::open(bootloaderpath)?, &mut file)?;
 
-    for name in names {
-        let mut command = Command::new("cargo")
-            .current_dir(name)
-            .args(&["build", "--message-format=json-render-diagnostics"])
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap();
+    let mut file = root.create_file("/kernel.elf")?;
+    io::copy(&mut File::open(kernelpath)?, &mut file)?;
 
-        let reader = BufReader::new(command.stdout.take().unwrap());
-        for message in Message::parse_stream(reader) {
-            match message.unwrap() {
-                Message::CompilerMessage(msg) => {
-                    // Pass messages straight back
-                    println!("{:?}", msg);
-                }
-                Message::CompilerArtifact(artifact) => match artifact.target.name.as_str() {
-                    "kernel" => {
-                        let mut file = partition.root_dir().create_file("/kernel.elf")?;
-                        io::copy(&mut File::open(&artifact.executable.unwrap())?, &mut file)?;
-                    }
-                    "bootloader" => {
-                        let mut file = partition.root_dir().create_file("/EFI/BOOT/BootX64.efi")?;
-                        io::copy(&mut File::open(&artifact.executable.unwrap())?, &mut file)?;
-                    }
-                    _ => (),
-                },
-                // We don't care about these
-                // Message::BuildScriptExecuted(script) => {
-                //     println!("{:?}", script);
-                // }
-                Message::BuildFinished(finished) => {
-                    println!("{:?}", finished);
-                    if !finished.success {
-                        return Err(anyhow!("Failed..."));
-                    }
-                }
-                _ => (), // Unknown message
-            }
-        }
-    }
-
-    let mut file = partition.root_dir().create_file("/startup.nsh")?;
-    io::copy(&mut File::open("startup.nsh")?, &mut file)?;
+    let mut file = root.create_file("/startup.nsh")?;
+    io::copy(&mut File::open("resources/startup.nsh")?, &mut file)?;
 
     // Launch qemu
     let _command = Command::new("qemu-system-x86_64")
@@ -110,7 +65,7 @@ fn main() -> anyhow::Result<()> {
             "-m",
             "128M",
             "-drive",
-            "if=pflash,format=raw,file=../OVMF/OVMF-pure-efi.fd",
+            "if=pflash,format=raw,file=OVMF/OVMF-pure-efi.fd",
             "-drive",
             "format=raw,file=CraftyOS.img",
         ])
@@ -118,4 +73,38 @@ fn main() -> anyhow::Result<()> {
         .unwrap();
 
     Ok(())
+}
+
+fn buildcargo(name: &str) -> Result<camino::Utf8PathBuf> {
+    // Cargo build args
+    let mut command = Command::new("cargo")
+        .current_dir(name)
+        .args(&["build", "--message-format=json-render-diagnostics"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    // Grab stdout and read it
+    let reader = BufReader::new(command.stdout.take().unwrap());
+    for message in Message::parse_stream(reader) {
+        match message.unwrap() {
+            // Pass messages straight back
+            Message::CompilerMessage(msg) => println!("{:?}", msg),
+
+            Message::CompilerArtifact(artifact) if artifact.target.name.as_str() == name => {
+                return Ok(artifact
+                    .executable
+                    .context("Invalid artifact executable path")?);
+            }
+            Message::BuildFinished(finished) => {
+                // Ensure successfull build
+                if !finished.success {
+                    return Err(anyhow!("Failed build of: {}", name));
+                }
+            }
+            _ => (), // Unknown message
+        }
+    }
+    // Function should have returned allready with the path
+    return Err(anyhow!("No Artifact found for: {}", name));
 }
